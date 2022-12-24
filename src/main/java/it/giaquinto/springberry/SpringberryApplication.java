11e0a88543aefac36427f397866a33e47dfc1d7b
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2022-12-24 10:20:16 +0100

    - Inital commit

---
+++ src/main/java/it/giaquinto/springberry/SpringberryApplication.java
@@ -0,0 +1,13 @@
+package it.giaquinto.springberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringberryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringberryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/giaquinto/springberry/SpringberryApplicationTests.java
@@ -0,0 +1,13 @@
+package it.giaquinto.springberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringberryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
