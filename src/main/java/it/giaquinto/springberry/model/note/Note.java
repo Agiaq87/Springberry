2c946c66fcf75d1efcf2bad0bf7c7a8c505138f3
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2022-12-27 09:42:08 +0100

    - User Dao and note

--- src/main/java/it/giaquinto/springberry/model/note/Note.java
+++ src/main/java/it/giaquinto/springberry/model/note/Note.java
@@ -1,12 +1,14 @@
 package it.giaquinto.springberry.model.note;
 
+import it.giaquinto.springberry.model.user.User;
+
 import java.time.LocalDateTime;
 
 public record Note(
         String name,
         String description,
-        LocalDateTime dateTime
+        LocalDateTime dateTime,
+        User user
 ) {
-
 
 }
---
+++ src/main/java/it/giaquinto/springberry/model/user/service/UserDaoService.java
@@ -0,0 +1,9 @@
+package it.giaquinto.springberry.model.user.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+
+}
