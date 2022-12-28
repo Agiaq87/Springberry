70baf0f5423e04fba004ba9fc362daea7781dbfe
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2022-12-28 20:25:16 +0100

    - Continue work on User

--- src/main/java/it/giaquinto/springberry/controller/implementation/UserController.java
+++ src/main/java/it/giaquinto/springberry/controller/implementation/UserController.java
@@ -6,6 +6,7 @@ import it.giaquinto.springberry.model.user.implementation.BaseUser;
 import it.giaquinto.springberry.model.user.implementation.GuestUser;
 import it.giaquinto.springberry.model.user.implementation.NormalUser;
 import it.giaquinto.springberry.model.user.service.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -46,27 +47,32 @@ public final class UserController {
     }
 
     @PostMapping(path = "/users")
-    public BaseUser createUser(@RequestBody final BaseUser baseUser) {
-        return userDaoService.makeNewUser(baseUser, SecurityLevel.GUEST);
+    public ResponseEntity<BaseUser> createUser(@RequestBody final BaseUser baseUser) {
+        userDaoService.makeNewUser(baseUser, SecurityLevel.GUEST);
+        return ResponseEntity.created(null).build();
     }
 
     @PostMapping(path = "/users/make/admin")
-    public BaseUser createAdminUser(@RequestBody final BaseUser baseUser) {
-        return userDaoService.makeNewUser(baseUser, SecurityLevel.ADMIN);
+    public ResponseEntity<BaseUser> createAdminUser(@RequestBody final BaseUser baseUser) {
+        userDaoService.makeNewUser(baseUser, SecurityLevel.ADMIN);
+        return ResponseEntity.created(null).build();
     }
 
     @PostMapping(path = "/users/make/power")
-    public BaseUser createPowerUser(@RequestBody final BaseUser baseUser) {
-        return userDaoService.makeNewUser(baseUser, SecurityLevel.POWER);
+    public ResponseEntity<BaseUser> createPowerUser(@RequestBody final BaseUser baseUser) {
+        userDaoService.makeNewUser(baseUser, SecurityLevel.POWER);
+        return ResponseEntity.created(null).build();
     }
 
     @PostMapping(path = "/users/make/normal")
-    public BaseUser createNormalUser(@RequestBody final BaseUser baseUser) {
-        return userDaoService.makeNewUser(baseUser, SecurityLevel.NORMAL);
+    public ResponseEntity<BaseUser> createNormalUser(@RequestBody final BaseUser baseUser) {
+        userDaoService.makeNewUser(baseUser, SecurityLevel.NORMAL);
+        return ResponseEntity.created(null).build();
     }
 
     @PostMapping(path = "/users/make/guest")
-    public BaseUser createGuestUser(@RequestBody final BaseUser baseUser) {
-        return userDaoService.makeNewUser(baseUser, SecurityLevel.GUEST);
+    public ResponseEntity<BaseUser> createGuestUser(@RequestBody final BaseUser baseUser) {
+        userDaoService.makeNewUser(baseUser, SecurityLevel.GUEST);
+        return ResponseEntity.created(null).build();
     }
 }
